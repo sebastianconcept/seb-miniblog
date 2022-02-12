fa7a6e33319c7fcb7a050bd67fdd56bf2d081de0
Sebastian Sastre <sebastianconcept@gmail.com>
2022-02-12 18:16:20 -0300

    edits side bar text

--- src/components/SidebarMenu/About.svelte
+++ src/components/SidebarMenu/About.svelte
@@ -1,6 +1,19 @@
 <script>
-
 </script>
+
+<section class="sidebar-section">
+  <h4>About</h4>
+  <p>
+    A blog about Smalltalk, Single Page Applications, and software engineering.
+    Here I occasionally share unrelated thoughts or insights that can help me or
+    someone in "connecting the dots".
+  </p>
+  <p>Also, some discoveries and insigths.</p>
+  <p>
+    Here is my
+    <a href="http://about.sebastiansastre.co">developer story</a>
+  </p>
+</section>
 
 <style>
   h4 {
@@ -11,17 +24,3 @@
     color: #fff;
   }
 </style>
-
-<section class="sidebar-section">
-  <h4>About</h4>
-  <p>
-    A blog about Smalltalk, Single Page Applications and software engineering.
-    Here I occasionaly share thoughts about creativity or psychology that I
-    think are worth spreading.
-  </p>
-  <p>Also, some discoveries and insigths.</p>
-  <p>
-    Here is my
-    <a href="http://about.sebastiansastre.co">developer story</a>
-  </p>
-</section>
